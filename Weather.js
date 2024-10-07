d8646dbc92e18c7d9d90a59f0bc3f026613f5bd8
zaynabraji <zaynab.raji@gmail.com>
2024-10-08 00:05:03 +0100

    js

--- Weather.js
+++ Weather.js
@@ -1,4 +1,28 @@
-const Apikey = 'fbe2fd7ab7819a343015d62b896d8804';
 function getWeather(){
+    const city = document.getElementById('city').value;
+    const Apikey = 'fbe2fd7ab7819a343015d62b896d8804';
+    const ApiUrl = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={fbe2fd7ab7819a343015d62b896d8804}';
     
+    fetch(ApiUrl)
+    .then(response => {
+        if (!response.ok) {
+          throw new Error('Not found');
+        }
+        return response.json();
+      })
+    .then(data => {
+        const weatherInfo = `
+      <h2>Weather in ${data.name}</h2>
+      <p><img src="cloud.png" alt="cloud and sun"/> ${data.main.temp}°C</p>
+      <p>Weather : ${data.weather[0].description}</p>
+      <p>Wind : ${data.wind.speed} km/h</p>
+      <p>Humidity : ${data.main.humidity} %</p>
+    `;
+    document.getElementById('weatherinfo').innerHTML = weatherInfo;
+    })
+
+    .catch(error => {
+        document.getElementById('weatherInfo').innerHTML = `<p>${error.message}</p>`;
+        // console.error('Erreur :', error);
+      });
 }
